4ec3da90098c7dd8be438d9905a5992f2b3408f4
Jan <jan.k62@gso.schule.koeln>
2023-11-10 10:52:56 +0100

    Klasse Buch erstellt

---
+++ Verlag/Buch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verlag
+{
+    class Buch
+    {
+        private string autor;
+        private string titel;
+        private int auflage;
+
+        public string Autor
+        {
+            set { autor = value; }get { return autor; }
+        }
+        public string Titel
+        {
+            set { titel = value; } get { return titel; }
+        }
+        public int Auflage
+        {
+            set { auflage = value; }
+            get { return auflage; }
+        }
+
+        public Buch(string autor, string titel, int auflage)
+        {
+            Autor = autor;
+            Titel = titel;
+            Auflage = auflage;
+        }
+    }
+}
